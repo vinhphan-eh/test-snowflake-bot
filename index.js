a9de4877a1d6a33a4da4cde5e84a24f0773bbd7e
Vinh Phan <vinhphan@employmenthero.com>
2024-12-29 14:21:20 +0700

    Add flow test file

---
+++ index.js
@@ -0,0 +1,64 @@
+// @flow
+import * as React from "react";
+import { Button, Empty, Card } from "@hero-design/rn";
+import styled from "@emotion/native";
+
+// Snowflakes using styled-components
+const StyledButton = styled(Button)`
+  padding: 10px;
+`;
+
+// @snowflake-guard/approved-styled-components
+const ApprovedStyledButton = styled(Button)`
+  padding: 10px;
+`;
+
+const StyledLinkButton = styled(Button.Icon)`
+  color: red;
+`;
+
+const { Icon } = Button;
+const StyledIcon = styled(Icon)`
+  color: red;
+`;
+
+const customBtnStyles: { [string]: mixed } = {
+  padding: 30,
+};
+
+const styles: { [string]: { [string]: mixed } } = {
+  btn: {
+    padding: 30,
+  },
+  badge: {
+    pt: 10,
+  },
+};
+
+const Sample = (): React.Node => (
+  <>
+    <StyledButton />
+    <StyledLinkButton />
+    <StyledIcon />
+    {/* Snowflakes using style prop */}
+    <Button style={{ width: 200 }} /> {/* acceptable */}
+    <Empty style={{ width: 200 }} />
+    <Card style={{ width: 200 }} /> {/* acceptable */}
+    <Button.Utility style={{ width: 200 }} />
+    <Button style={customBtnStyles} />
+    <Button variant="text" style={{ ...customBtnStyles }} />
+    <Button style={styles.btn} />
+    <Button style={{ ...styles.btn }} />
+    <Icon style={{ padding: 20 }} />
+    {/* @snowflake-guard/approved-inline-style attributes: height, padding */}
+    <Icon style={{ padding: 20, height: 40 }} />
+    <Icon
+      style={{
+        padding: 20,
+        backgroundColor: "red",
+      }}
+    />
+  </>
+);
+
+export default Sample;
